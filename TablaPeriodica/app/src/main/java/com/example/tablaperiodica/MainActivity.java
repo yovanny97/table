c71af284b95ea4d53cd796d9c38e5f8545ee1dbd
Alberto Fabian <lf_alberto@hotmail.com>
2019-10-15 23:57:58 -0500

    1 commit

--- TablaPeriodica/app/src/main/java/com/example/tablaperiodica/MainActivity.java
+++ TablaPeriodica/app/src/main/java/com/example/tablaperiodica/MainActivity.java
@@ -3,10 +3,17 @@ package com.example.tablaperiodica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.RadioButton;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     RadioButton rbazul,rbrojo,rbverde;
@@ -14,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     Button btn;
     ListView list;
     String Nombre,Simbolo,NumeroA,Masa,Grupo;
+    ArrayList<String> elementList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +41,41 @@ public class MainActivity extends AppCompatActivity {
 
         list=(ListView)findViewById(R.id.lista);
 
+        leerArchivo();
+
+    }
+
+    private void leerArchivo(){
+        try{
+
+            InputStream file = getResources().openRawResource(R.raw.datos);
+            InputStreamReader reader = new InputStreamReader(file);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            String line = bufferedReader.readLine();
+
+            while (line != null){
+                String [] element = line.split(":");
+
+
+                elementList.add(element[1]);
+
+                line = bufferedReader.readLine();
+
+
+            }
+
+            for (String l : elementList){
+                Log.i("Elements",l);
+            }
+
+            ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,elementList);
+            list.setAdapter(arrayAdapter);
+
+
+
+        }catch (Exception e){
+
+        }
     }
 }
